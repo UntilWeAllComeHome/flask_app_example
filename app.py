d456d324ef4c5906c7cd6a631c79f79c15a72af5
UWACH <tanner.overcash@gmail.com>
2023-03-29 09:13:05 -0600

    Updates to API

--- app.py
+++ app.py
@@ -4,40 +4,50 @@ from shapely.geometry import Polygon
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'General API description here.'
+@app.route('/ping')
+def ping_pong():
+    return 'pong'
 
 @app.route('/calculate_center/<path:filepath>')
 def calculate_center(filepath):
     """Calculates the center of a passed geosjon file."""
 
-    gdf = gpd.read_file(('/' + filepath))
-    center = gdf.dissolve().centroid
-    return(str(center.to_json()))
+    try:
+        gdf = gpd.read_file(('/' + filepath))
+        center = gdf.dissolve().centroid
+        return(str(center.to_json()))
+    except:
+        return("Error: File not found or file is not a spatial file.")
 
 @app.route('/calculate_bounds/<path:filepath>')
 def calculate_bounds(filepath):
     """Calculates the total bounds of a passed geosjon file."""
 
-    gdf = gpd.read_file(('/' + filepath))
-    t_bounds = gdf.total_bounds
-    return(str(t_bounds))
+    try:
+        gdf = gpd.read_file(('/' + filepath))
+        t_bounds = gdf.total_bounds
+        return(str(t_bounds))
+    except:
+        return("Error: File not found or file is not a spatial file.")
 
 @app.route('/calculate_boundry/<path:filepath>')
 def calculate_boundry(filepath):
     """Calculates the total bounds of a passed geosjon file."""
 
-    gdf = gpd.read_file(('/' + filepath))
-    bound = gdf.boundary
-
-    return(str(bound.to_json()))
+    try:
+        gdf = gpd.read_file(('/' + filepath))
+        bound = gdf.boundary
+        return(str(bound.to_json()))
+    except:
+        return("Error: File not found or file is not a spatial file.")
 
 @app.route('/calculate_area/<path:filepath>')
 def calculate_area(filepath):
     """Calculates the total bounds of a passed geosjon file."""
-
-    gdf = gpd.read_file(('/' + filepath))
-    area = gdf.area
-
-    return(str(area))
+    
+    try:
+        gdf = gpd.read_file(('/' + filepath))
+        area = gdf.area
+        return(str(area))
+    except:
+        return("Error: File not found or file is not a spatial file.")
